6a64703e1801caf751172aa195dd650f6d620abc
MylenaGabrielle <mylenagabrielle1@gmail.com>
2025-07-14 09:27:07 +0000

    14/07

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Mylena
+website project for foundation of web development 2025
---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include "includes/head.php"; ?>
+
+<body>
+    <?php include "includes/pageheader.php" ?>
+    <div class="home-banner">
+        <div class="banner-text">
+            <h2>SALE</h2>
+            <p>Get massive discounts in our mid-year sale</p>
+            <a href="sale.html">Shop</a>
+        </div>
+    </div>
+    <main class="container">
+        <div class="card">
+            <img src="products/women accessories.png" alt="women with accessories">
+            <div class="card-content">
+                <h4 class="card-title">Product 1</h4>
+                <p class="price">199</p>
+                <a href="#" class="card-button">
+                    Add to cart
+                </a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="products/gym clothes women.avif" alt="a woman wearing gym clothes">
+            <div class="card-content">
+                <h4 class="card-title">Product 2</h4>
+                <p class="price">299</p>
+                <a href="#" class="card-button">
+                    Add to cart
+                </a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="products/gloves for women.webp" alt="women wearing gloves">
+            <div class="card-content">
+                <h4 class="card-title">Product 3</h4>
+                <p class="price">99</p>
+                <a href="#" class="card-button">
+                    Add to cart
+                </a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="products/swimming clothes for women.webp" alt="a woman wearing swimming clothes">
+            <div class="card-content">
+                <h4 class="card-title">Product 4</h4>
+                <p class="price">199</p>
+                <a href="#" class="card-button">
+                    Add to cart
+                </a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="products/workout clothes for women.avif" alt="a women using workout clothes">
+            <div class="card-content">
+                <h4 class="card-title">Product 5</h4>
+                <p class="price">199</p>
+                <a href="#" class="card-button">
+                    Add to cart
+                </a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="products/women football clothing nike.avif" alt="a woman wearing football clothing nike">
+            <div class="card-content">
+                <h4 class="card-title">Product 6</h4>
+                <p class="price">199</p>
+                <a href="#" class="card-button">
+                    Add to cart
+                </a>
+            </div>
+        </div>
+        <div class="bottom-banner">
+            <div class="banner-text">
+                <h2>Clearance Sale</h2>
+                <p>Get massive discounts</p>
+                <a href="clearance.html">Shop Clearance</a>
+            </div>
+        </div>
+    </main>
+    <?php include "includes/footer.php"; ?>
+</body>
+
+</html>
